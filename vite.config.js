8333ba409b8631e617826bd63ef922034bd5a445
Shadow393659 <abdulrahmanr861@gmail.com>
2025-12-12 23:19:18 +0530

    Fix quotes in vite.config.js base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/noordeen-portfolio/",
+  base: '/noordeen-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
