94e293e933d0b4a60a60241a2553ed1bebaa9250
Shadow393659 <abdulrahmanr861@gmail.com>
2025-12-13 00:34:18 +0530

    Save local changes before merge

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/noordeen-portfolio/' : '/',,
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"), // <-- crucial
     },
   },
+  base: "./", // important for GitHub Pages
 });
